29e625fb0b13df3befe4c49a8b927aa4e9abab08
ziyuyx <52502143+ziyuyx@users.noreply.github.com>
2022-01-07 17:13:38 +0800

    上传项目
    上传项目

---
+++ src/main/java/com/yx/secondkill/SecondkillApplication.java
@@ -0,0 +1,13 @@
+package com.yx.secondkill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondkillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondkillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yx/secondkill/SecondkillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yx.secondkill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondkillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
